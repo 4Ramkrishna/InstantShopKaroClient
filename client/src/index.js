69db635f2f81c2a8b850d87c21bc01107d062f59
4Ramkrishna <gs2110045@sgsitsindore.in>
2024-07-17 17:33:30 +0530

    v1
    v1

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from 'axios';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,6 +9,8 @@ import { AuthProvider } from './context/auth';
 import { SearchProvider } from './context/search';
 import "antd/dist/reset.css";
 import { CartProvider } from './context/cart';
+
+axios.defaults.baseURL = 'https://instantshopkaroserver.onrender.com';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
