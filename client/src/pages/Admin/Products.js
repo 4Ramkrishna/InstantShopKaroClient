37d6cdb4ac687432e3133f3197a52652a231020b
4Ramkrishna <gs2110045@sgsitsindore.in>
2024-07-24 20:39:51 +0530

    v3
    v3

--- client/src/pages/Admin/Products.js
+++ client/src/pages/Admin/Products.js
@@ -37,7 +37,7 @@ const Products = () => {
                     >
                         <div className="card m-2" style={{ width: "18rem" }}>
                         <img
-                            src={`/api/v1/product/product-photo/${p._id}`}
+                            src={`https://instantshopkaroserver.onrender.com/api/v1/product/product-photo/${p._id}`}
                             className="card-img-top"
                             alt={p.name}
                         />
